5d993df84b7b2c1e44a495aa77612aed4dc9ed2d
dabliuw22 <wleylop22@gmail.com>
2018-01-23 16:15:37 -0500

    Agregando lógica para comportamiento paralelo

--- src/main/java/com/leysoft/sumavector/App.java
+++ src/main/java/com/leysoft/sumavector/App.java
@@ -2,12 +2,13 @@ package com.leysoft.sumavector;
 
 import com.leysoft.sumavector.resorces.Recurso;
 import com.leysoft.sumavector.threads.Worker;
+import com.leysoft.sumavector.threads.WorkerParallel;
 
 public class App {
 	
 	private static final int NUM_HILOS = 4;
 	
-	private static final int SALTO = 4;
+	private static final int SALTO = 100;
 	
 	private static int[] ARRAY = new int[NUM_HILOS*SALTO];
 	
@@ -18,28 +19,49 @@ public class App {
 	}
 	
     public static void main( String[] args ) {
-        Thread[] hilos = new Thread[NUM_HILOS];
+        Thread[] hilosEM = new Thread[NUM_HILOS];
+        Thread[] hilosP = new Thread[NUM_HILOS];
         llenarArray();
         Recurso recurso = new Recurso(ARRAY);
     	
+        /* INICIO EXCLUSIÓN MUTUA */
         for(int i = 0; i < NUM_HILOS; i++) {
-        	hilos[i] = new Thread(new Worker(i*recurso.getArray().length/NUM_HILOS, 
+        	hilosEM[i] = new Thread(new Worker(i*recurso.getArray().length/NUM_HILOS, 
         			i*recurso.getArray().length/NUM_HILOS + recurso.getArray().length/NUM_HILOS, recurso), 
-        			"hilo[" + Integer.toString(i) + "]");
+        			"hiloEM[" + Integer.toString(i) + "]");
         }
         
         for(int i = 0; i < NUM_HILOS; i++) {
-        	hilos[i].start();
+        	hilosEM[i].start();
         }
+        /* FIN EXCLUSIÓN MUTUA */
+        
+        /* INICIO EJECUCIÓN PARALELA */
+        for(int i = 0; i < NUM_HILOS; i++) {
+        	hilosP[i] = new WorkerParallel(ARRAY, i*ARRAY.length/NUM_HILOS, 
+        			i*ARRAY.length/NUM_HILOS + ARRAY.length/NUM_HILOS, "hiloP[" + Integer.toString(i) + "]");
+        }
+        
+        for(int i = 0; i < NUM_HILOS; i++) {
+        	hilosP[i].start();
+        }
+        
+        /* FIN EJECUCIÓN PARALELA */
         
         for(int i = 0; i < NUM_HILOS; i++) {
         	try {
-				hilos[i].join();
+				hilosEM[i].join();
+				hilosP[i].join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
         }
         
+        int suma = 0;
+        for(int i = 0; i < NUM_HILOS; i++) {
+        	suma += ((WorkerParallel) hilosP[i]).getSuma();
+        }
+        System.out.println("Parallel: suma() = " + suma);
         System.out.println("Resource: suma() = " + recurso.getSuma());
     }
 }
---
+++ src/main/java/com/leysoft/sumavector/threads/WorkerParallel.java
@@ -0,0 +1,33 @@
+package com.leysoft.sumavector.threads;
+
+public class WorkerParallel extends Thread {
+
+	private int[] array;
+	private int inicio;
+	private int fin;
+	private int suma;
+	
+	public WorkerParallel(int[] array, int inicio, int fin, String nombre) {
+		super(nombre);
+		this.array = array;
+		this.inicio = inicio;
+		this.fin = fin;
+		this.suma = 0;
+	}
+
+	public int[] getArray() {
+		return array;
+	}
+
+	public int getSuma() {
+		return suma;
+	}
+
+	@Override
+	public void run() {
+		for(int i = this.inicio; i < this.fin; i++) {
+			this.suma += this.array[i];
+		}
+		System.out.println("PARAREL -> suma("+ this.inicio +", "+ this.fin +")..." + Thread.currentThread().getName());
+	}
+}
